952743dbfd12231386f2ff24a974d332e33c29cd
redhatlinux10 <bruce.oy@gmail.com>
2015-07-25 18:30:40 +0800

    当 RPM 已经下载的时候，无需重新下载。

--- Oracle-JDK-Installer.sh
+++ Oracle-JDK-Installer.sh
@@ -15,17 +15,18 @@ JDK_INSTALLED_RPM_COUNT=`rpm -qa|grep $JDK_RPM_NAME|wc -l`
 
 if [ "$JDK_INSTALLED_RPM_COUNT" == "0" ]
 then
-  if [ "$ARIA2C_INSTALLED" == "1" ]
-  then
-    aria2c -c -d ~ -x 10 -s 10 --check-certificate=false --header="Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk8-downloads-2133151.html; oraclelicense=accept-securebackup-cookie; s_cc=true; s_sq=%5B%5BB%5D%5D" "$JDK_DL_URL"
-  else
-    wget -c -p ~ --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk8-downloads-2133151.html; oraclelicense=accept-securebackup-cookie; s_cc=true; s_sq=%5B%5BB%5D%5D" "$JDK_DL_URL"
+  if [ ! -f ~/$JDK_FILE_NAME ] ; then 
+    if [ "$ARIA2C_INSTALLED" == "1" ]
+    then
+      aria2c -c -d ~ -x 10 -s 10 --check-certificate=false --header="Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk8-downloads-2133151.html; oraclelicense=accept-securebackup-cookie; s_cc=true; s_sq=%5B%5BB%5D%5D" "$JDK_DL_URL"
+    else
+      wget -c -p ~ --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk8-downloads-2133151.html; oraclelicense=accept-securebackup-cookie; s_cc=true; s_sq=%5B%5BB%5D%5D" "$JDK_DL_URL"
+    fi
   fi
   
   sudo rpm -Uvh --nodeps ~/$JDK_FILE_NAME
   JAVA_BIN_PATH=`rpm -ql $JDK_RPM_NAME|grep -E '[0-9]+/bin/java$'`
   JAVA_PLUGIN_LIB_PATH=`rpm -ql $JDK_RPM_NAME|grep -E '/libnpjp2.so$'`
-
   # 获取已经安装的 Java 的 alternative 的最大优先级并加一（忽略不包含 priority 的行）
   JAVA_BIN_CUR_PRI=`/usr/sbin/update-alternatives --display java|awk '{if($0!~/priority/)next}{max=(max>$4)?max:$4}END{print max+1}'`
   JAVA_PLUGIN_CUR_PRI=`/usr/sbin/update-alternatives --display javaplugin|awk '{if($0!~/priority/)next}{max=(max>$4)?max:$4}END{print max+1}'`    
